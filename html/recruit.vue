bad30776a320df46b19ed8eb6833ecea7f4d6190
motty0533 <soutaro.ym@gmail.com>
2021-10-30 11:37:54 +0900

    デザイン修正・Googleフォーム埋め込み

--- html/recruit.vue
+++ html/recruit.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <h2>新規委員応募</h2>
-    ※ここにGoogleフォームを埋め込み
+    <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSeGHqRdQttPHsXAD_Ph4q-_xm112vpDxt7kl5tkHquFruc4yw/viewform?embedded=true"
+      width="100%"
+      height="1800"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      >読み込んでいます…</iframe
+    >
   </div>
 </template>

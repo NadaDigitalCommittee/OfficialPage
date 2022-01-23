70bd917f5314b4130f4213085abfba59f709026d
motty0533 <soutaro.ym@gmail.com>
2022-01-24 02:03:15 +0900

    cms starter

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,15 @@
 </template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
+  },
+}
 </script>
 
 <style lang="scss">

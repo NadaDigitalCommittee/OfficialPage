2046c5aff3b663a5020af7e39422a3610523fa6b
motty0533 <soutaro.ym@gmail.com>
2022-01-24 00:32:51 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       {
         rel: 'stylesheet',

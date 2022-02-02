67ea992de70e8fcbe6c967437c5cc979e36c3089
motty0533 <soutaro.ym@gmail.com>
2022-02-02 16:22:23 +0900

    活動内容組織情報各機関紹介追記

---
+++ components/Movieicon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="movie">動画課</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.movie {
+  padding: 10px;
+  color: #fff;
+  font-weight: bold;
+  background-color: #dc6b9a;
+  border-radius: 10px;
+  display: block;
+  width: max-content;
+}
+</style>
---
+++ components/Systemicon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="system">情シス課</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.system {
+  padding: 10px;
+  color: #fff;
+  font-weight: bold;
+  background-color: #00a1e9;
+  border-radius: 10px;
+  display: block;
+  width: max-content;
+}
+</style>

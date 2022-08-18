2e51026cad91ddf6a46ba2f90e77bb0bc1accd28
Googlefan256 <yyykakeru@gmail.com>
2022-08-18 13:53:43 +0900

    :bug: un optimized images

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@ const nextConfig = withMDX({
 	pageExtensions: ["tsx", "mdx"],
 	reactStrictMode: true,
 	swcMinify: true,
-	images: {
-		unoptimized: true,
-	}
+	experimental: {
+		images: {
+			unoptimized: true,
+		},
+	},
 });
 
 module.exports = nextConfig;
